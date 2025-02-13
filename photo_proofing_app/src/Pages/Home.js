41a56c1fe11721b4771629c18548f6d9dad69dab
KarimIsaac <90776600+KarimIsaac@users.noreply.github.com>
2025-02-13 14:53:10 +0100

    Styling

--- photo_proofing_app/src/Pages/Home.js
+++ photo_proofing_app/src/Pages/Home.js
@@ -1,12 +1,11 @@
 import TitleRename from "../utility/TitleRename";
-import Slideshow from "../Components/Slideshow";
 import Items from "../Components/Items";
 const Home = () => {
   TitleRename("Photo Proof - Home");
   //Home Page
   return (
     <div id="homeDiv">
-      <Slideshow/>
+    
       <Items/>
       </div>  
   );
