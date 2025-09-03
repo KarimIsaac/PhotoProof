2b25aeb533ce8108241a30e0636af3883dd48faf
KarimIsaac <90776600+KarimIsaac@users.noreply.github.com>
2025-09-03 23:48:22 +0200

    small fixes

--- photo_proofing_app/src/Pages/Home.js
+++ photo_proofing_app/src/Pages/Home.js
@@ -1,8 +1,6 @@
 import TitleRename from "../utility/TitleRename";
-<<<<<<< HEAD
 import Register from "./Register";
-=======
->>>>>>> 41a56c1fe11721b4771629c18548f6d9dad69dab
+
 import Items from "../Components/Items";
 import { Route } from "react-router-dom";
 import Footer from "../Components/Footer";
@@ -12,7 +10,6 @@ const Home = () => {
   //Home Page
   return (
     <div id="homeDiv">
-<<<<<<< HEAD
       <div className="first-page"></div>
       <div className="first-text">
         <h1>PHOTO<span id="first-text2">PROOF</span></h1>
@@ -20,9 +17,9 @@ const Home = () => {
         <p className="right-text">Pellentesque mauris a lobortis in bibendum sed lobortis semper. Eget eu vel eu vitae.</p>
       </div>
       <Footer />
-=======
+
     
->>>>>>> 41a56c1fe11721b4771629c18548f6d9dad69dab
+
       <Items/>
       <About/>
       </div>  
